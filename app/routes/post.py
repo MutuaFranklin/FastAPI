fb604d1d2920aba7b77153e71497878af9e856a8
Franklin Ngumbi Mutua <franklin.mutua@un.org>
2023-11-25 15:48:31 +0300

    directly unpack the tuple in the query result and use post and votes without introducing an intermediate variable.

--- app/routes/post.py
+++ app/routes/post.py
@@ -13,13 +13,39 @@ router = APIRouter(
     tags=['Posts']
 )
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.PostResponse])
+@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.PostVotesResponse])
 def get_my_posts(db: Session=Depends(get_db), current_user: int =Depends(oauth2.get_current_user)):
-    posts=db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    # posts=db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
     # print(current_user.id)
-    return posts
+    posts = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .outerjoin(models.Vote, models.Vote.post_id == models.Post.id)
+        .filter(models.Post.owner_id == current_user.id)
+        .group_by(models.Post.id)
+        .all()
+    )
+  # Convert the tuple into a list of PostVotesResponse Pydantic models
+    results = [
+        schemas.PostVotesResponse(
+            post=schemas.PostResponse(
+                id=post.id,
+                title=post.title,
+                content=post.content,
+                published=post.published,
+                created_at=post.created_at,
+                owner=schemas.UserResponse(
+                    id=post.owner.id,
+                    email=post.owner.email,
+                    created_at=post.owner.created_at,
+                ),
+            ),
+            votes=votes,
+        )
+        for post, votes in posts
+    ]
 
-#@router.get("/all", response_model=List[dict], status_code=status.HTTP_200_OK)
+    return results
+
 @router.get("/all", response_model=List[schemas.PostVotesResponse], status_code=status.HTTP_200_OK)
 def get_all_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 20, skip: int = 0, search: Optional[str] = "",
 ):
@@ -67,14 +93,38 @@ def create_post(post: schemas.CreatePost, db: Session =Depends(get_db), current_
     return new_post
 
 
-@router.get("/{id}",  response_model=schemas.PostResponse)
+@router.get("/{id}",  response_model=schemas.PostVotesResponse)
 def get_post(id:int, db: Session =Depends(get_db), current_user: int =Depends(oauth2.get_current_user)):    
-    post=db.query(models.Post).filter(models.Post.id == id).first()  
-    
+    #directly unpack the tuple in the query result and use post and votes without introducing an intermediate variable.
+    post, votes = (
+        db.query(models.Post, func.count(models.Vote.post_id).label("votes"))
+        .outerjoin(models.Vote, models.Vote.post_id == models.Post.id)
+        .filter(models.Post.id == id)
+        .group_by(models.Post.id)
+        .first()
+    )
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail =f"Post with id: {id} was not found")
+    
+    # Convert the tuple into PostVotesResponse Pydantic models
+    post_response = schemas.PostVotesResponse(
+        post=schemas.PostResponse(
+            id=post.id,
+            title=post.title,
+            content=post.content,
+            published=post.published,
+            created_at=post.created_at,
+            owner=schemas.UserResponse(
+                id=post.owner.id,
+                email=post.owner.email,
+                created_at=post.owner.created_at,
+            )
+        ),
+        votes=votes,
+    )
           
-    return post
+    return post_response
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
